4d94d1955336cc6dfa4eebc7a368a9f9cdc4055e
Jesse Borries <jesse.borries@Gmail.com>
2017-07-10 09:46:55 -0500

    comments added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-
-var my_module = require('./my_module')();     //notice the extra invocation parentheses!
+//"Require" is essentially importing javascript from another file
+var my_module = require('./my_module')();     //notice the extra invocation parentheses to be able to import a function! Without the (), you can just pull in an object
+//Also, there is no .js at the end of the file. The require method automatically looks for JavaScript files so we don't need to include a file extension.
 console.log(my_module);
 my_module.greet();
 my_module.add(5,6);
+
+//By default, the require() method looks for the modules located in a folder called node_modules. To tell require() to look in the current directory that app.js is also in, we have to include "./" in front of the file path.
--- my_module.js
+++ my_module.js
@@ -1,4 +1,7 @@
-module.exports = function(){
+//Requiring a Node module allows you to use the module.exports object of another file!
+
+module.exports = function(){ //this object constructor gets exported to the app.js file
+//we can delete the function() and it's just an object we're exporting at that point.
     return {
         greet: function(){
             console.log("we are now using a module!");
